fc38c8efab3271be21f15ad12a42169369bc7706
Yash Goswami <yashgoswami148@gmail.com>
2022-04-19 20:37:32 +0530

    question 27 complete

---
+++ src/linked_list/question_27/Node.java
@@ -0,0 +1,12 @@
+package linked_list.question_27;
+
+public class Node {
+    int data;
+    Node right,down;
+
+    Node(int data){
+        this.data = data;
+        right = null;
+        down = null;
+    }
+}
