9e1d71e6d9ea19130e3251e626b1fa0236ad30e2
Yash Goswami <yashgoswami148@gmail.com>
2022-04-27 13:18:03 +0530

    updated file

--- src/BinaryTree/heightOfTree.js
+++ src/BinaryTree/heightOfTree.js
@@ -23,10 +23,36 @@ function height(node) {
   }
 }
 
+function heightUsingQueue() {
+  let q = [];
+  q.push(root);
+  q.push(null);
+
+  let count = 0;
+  while (q.length !== 0) {
+    let temp = q.shift();
+
+    if (temp === null) {
+      count++;
+      if (q.length !== 0) {
+        q.push(null);
+      }
+    } else {
+      if (temp.left !== null) {
+        q.push(temp.left);
+      }
+      if (temp.right !== null) {
+        q.push(temp.right);
+      }
+    }
+  }
+  return count;
+}
+
 root = new Node(1);
 root.left = new Node(2);
 root.right = new Node(3);
 root.left.left = new Node(4);
 root.left.right = new Node(5);
 
-console.log(`Height of tree: ${height(root)}`);
+console.log(`Height of tree: ${heightUsingQueue()}`);
